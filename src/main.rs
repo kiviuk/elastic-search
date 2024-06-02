d699675b654b8e5ec2197775627a0c9abca58229
Mobe <mo@be.org>
2024-06-02 20:34:12 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -31,37 +31,3 @@ fn main() {
         println!("{}", serde_json::to_string(&data).unwrap());
     }
 }
-// fn main() {
-//     let mut lines = String::new();
-//     io::stdin().read_to_string(&mut lines).unwrap();
-//
-//     let mut data: Vec<Value> = vec![];
-//
-//     for (i, line) in lines.lines().enumerate() {
-//         // Skip if the line doesn't start with [
-//         if !line.starts_with("[") {
-//             continue;
-//         }
-//
-//         let parts: Vec<&str> = line.split("] ").collect();
-//         let times: Vec<&str> = parts[0][1..].split("s -> ").collect();
-//         let text = parts[1];
-//
-//         let start_time = times[0];
-//         let end_time = times[1];
-//
-//         data.push(json!({
-//             "start_time": start_time,
-//             "end_time": end_time,
-//             "text": text
-//         }));
-//     }
-//     let index_instruction = json!({"index": {"_index": "gsp"}});
-//
-//     println!(
-//         "{}",
-//         serde_json::to_string_pretty(&index_instruction).unwrap()
-//     );
-//     println!("{}", serde_json::to_string_pretty(&data).unwrap());
-//     // println!("{}", serde_json::to_string_pretty(&data).unwrap());
-//}
